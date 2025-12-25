aa1ad8341d8e9a05146e6948c60226f8a7087684
LIN Zhaorun <lzr5198@gmail.com>
2025-12-25 14:19:13 +0800

    change single line comment

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,27 +3,23 @@
    ========================================================================== */
 
 $(document).ready(function(){
-  // Parallax scroll for contained hero
+  /* Parallax scroll for contained hero */
   var $heroContained = $(".page__hero-contained");
   if ($heroContained.length) {
     var updateParallax = function() {
       var scrollTop = $(window).scrollTop();
       var heroOffset = $heroContained.offset().top;
       var heroHeight = $heroContained.outerHeight();
-      
-      // Calculate scroll progress (0 to 1)
+      /* Calculate scroll progress (0 to 1) */
       var scrollProgress = scrollTop / (heroOffset + heroHeight);
       scrollProgress = Math.max(0, Math.min(1, scrollProgress));
-      
-      // Start at 20% (upper-middle), move to 100% (bottom) as we scroll
+      /* Start at 20% (upper-middle), move to 100% (bottom) as we scroll */
       var bgPosition = 20 + (scrollProgress * 80);
       $heroContained.css("background-position", "center " + bgPosition + "%");
     };
-    
-    // Set initial position immediately
+    /* Set initial position immediately */
     updateParallax();
-    
-    // Update on scroll
+    /* Update on scroll */
     $(window).on("scroll", updateParallax);
   }
 
